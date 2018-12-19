e48d27d433cb56a9677e9223ee8f735ad7d82f8a
reubwn <reubennowell@gmail.com>
2018-12-19 16:03:59 +0000

    update

--- parse_RM_out.pl
+++ parse_RM_out.pl
@@ -42,53 +42,127 @@ GetOptions (
 );
 ## help and usage
 die $usage if $help;
-die $usage unless ($RM_infile);
 print STDERR "[####] TE-EVOLUTION parse_RM_out.pl\n";
 print STDERR "[####] " . `date`;
 
-## STUFF
-my %print_hash;
-my $genome_length;
+###############
+## single files
+###############
+if ($RM_infile) {
 
-if ($fasta_infile) {
-  open (my $FASTA, $fasta_infile) or die $!;
-  while (<$FASTA>) {
-    if ($_ =~ m/^\>/) {
+  ## STUFF
+  my %print_hash;
+  my $genome_length;
+
+  if ($fasta_infile) {
+    open (my $FASTA, $fasta_infile) or die $!;
+    while (<$FASTA>) {
+      if ($_ =~ m/^\>/) {
+        next;
+      } else {
+        chomp;
+        $genome_length += length ($_);
+      }
+    }
+    print STDERR "[####] Genome length: $genome_length bp\n";
+  }
+
+  open (my $IN, $RM_infile) or die $!;
+  while (<$IN>) {
+    chomp;
+    my $line = trim ($_); ## remove leading/trailing whitespace
+    if ($line !~ m/^\d+/) {
       next;
     } else {
+      my @F = split (/\s+/, $line);
+      my $rep = $F[10];
+      unless ($no_condense) {
+        $rep =~ s/-.*//;
+        $rep =~ s/\?//g;
+      }
+      $print_hash{$rep} += ( $F[6] - $F[5] + 1 );
+    }
+  }
+  close $IN;
+
+  foreach (sort { "\L$a" cmp "\L$b" || $print_hash{$b} <=> $print_hash{$a} } keys %print_hash) {
+    print STDOUT join ("\t", $_, $print_hash{$_});
+    if ($fasta_infile) {
+      print "\t" . (($print_hash{$_}/$genome_length)*100) . "\n";
+    } else {
+      print "\n";
+    }
+  }
+
+######################
+## glob multiple files
+######################
+} else {
+
+  ## STUFF
+  my %files_hash;
+  my %repeats_hash;
+  my %genome_lengths_hash;
+
+  my @files = glob "*.out";
+  print STDERR "[####] Number of RM files: ".@files."\n";
+
+  foreach my $file (@files) {
+    print STDERR "[####] Working on file: $file\n";
+    ## STUFF
+    my %print_hash;
+    (my $fasta_infile = $file) =~ s/\.out//;
+    my $genome_length;
+
+    open (my $FASTA, $fasta_infile) or die $!;
+    while (<$FASTA>) {
+      if ($_ =~ m/^\>/) {
+        next;
+      } else {
+        chomp;
+        $genome_length_perfile += length ($_);
+      }
+    }
+    print STDERR "[####] Genome length: $genome_length bp\n";
+    $genome_lengths_hash{$file} = $genome_length;
+
+    open (my $IN, $file) or die $!;
+    while (<$IN>) {
       chomp;
-      $genome_length += length ($_);
+      my $line = trim ($_); ## remove leading/trailing whitespace
+      if ($line !~ m/^\d+/) {
+        next;
+      } else {
+        my @F = split (/\s+/, $line);
+        my $rep = $F[10];
+        unless ($no_condense) {
+          $rep =~ s/-.*//;
+          $rep =~ s/\?//g;
+        }
+        $print_hash{$rep} += ( $F[6] - $F[5] + 1 );
+        $repeats_hash{$rep}++;
+      }
     }
+    close $IN;
+    $files_hash{$file} = \%print_hash;
   }
-  print STDERR "[####] Genome length: $genome_length bp\n";
+
+  print STDOUT join ("\t", "\t", nsort keys %files_hash);
+  foreach my $repeat (nsort keys %repeats_hash) {
+    print STDOUT "$repeat\t";
+    foreach my $file (nsort keys %files_hash) {
+      my %print_hash = %{ $files_hash{$_} };
+      if ($print_hash{$repeat} ) {
+        print STDOUT (($print_hash{$repeat}/$genome_lengths_hash{$file})*100) . "\t";
+      } else {
+        print STDOUT "0\t";
+      }
+    }
+    print STDOUT "\n";
+  }
 }
 
-open (my $IN, $RM_infile) or die $!;
-while (<$IN>) {
-  chomp;
-  my $line = trim ($_); ## remove leading/trailing whitespace
-  if ($line !~ m/^\d+/) {
-    next;
-  } else {
-    my @F = split (/\s+/, $line);
-    my $rep = $F[10];
-    unless ($no_condense) {
-      $rep =~ s/-.*//;
-      $rep =~ s/\?//g;
-    }
-    $print_hash{$rep} += ( $F[6] - $F[5] + 1 );
-  }
-}
-close $IN;
 
-foreach (sort { "\L$a" cmp "\L$b" || $print_hash{$b} <=> $print_hash{$a} } keys %print_hash) {
-  print STDOUT join ("\t", $_, $print_hash{$_});
-  if ($fasta_infile) {
-    print "\t" . (($print_hash{$_}/$genome_length)*100) . "\n";
-  } else {
-    print "\n";
-  }
-}
 
 ############ SUBS
 
