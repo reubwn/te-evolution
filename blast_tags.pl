4cb6d10a5192228ff41e1a0a6215b0393e315b45
reubwn <reubennowell@gmail.com>
2019-03-21 19:05:36 +0100

    update

--- blast_tags.pl
+++ blast_tags.pl
@@ -91,7 +91,7 @@ close $SAM;
 ## print some information
 if ( $verbose ) {
   foreach (nsort keys %ltr_hash) {
-    my $lefties = @{$ltr_hash{$_}{left_names}} ? scalar(@{$ltr_hash{$_}{left_names}}) : 0;
+    my $lefties = (@{$ltr_hash{$_}{left_names}}) ? scalar(@{$ltr_hash{$_}{left_names}}) : 0;
     my $righties = @{$ltr_hash{$_}{right_names}} ? scalar(@{$ltr_hash{$_}{right_names}}) : 0;
     print STDERR "[INFO] $_ has $lefties left reads and $righties right reads\n";
   }
