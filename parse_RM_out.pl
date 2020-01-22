7beca82cd04e591210af7bf787132fcfa61f5803
Reuben Nowell <reubennowell@gmail.com>
2020-01-22 14:44:49 +0100

    update

--- parse_RM_out.pl
+++ parse_RM_out.pl
@@ -14,24 +14,20 @@ use Data::Dumper;
 
 my $usage = "
 SYNOPSIS
+  Globs *.out (or *.out.gz) RM files and summarizes counts across all input files.
+  Requires a genome span mapping file in 'filename [TAB] genome_size' format.
 
 OPTIONS [*] = required
-  -i|--infile  [FILE]* : RepeatMasker out file (*.out)
-  -f|--fasta   [FILE]* : Genome fasta file
-  -m|--mapping [FILE]  : Mapping file with genome sizes for each file
-  -g|--glob            : Will glob all *.out files and attempt to process them
-  -n|--nocondense      : Classify based on subfamilies [family]
+  -m|--mapping [FILE]  : mapping file with genome sizes for each file
+  -z|--gz              : gunzip any *.out.gz files prior to glob
+  -n|--nocondense      : classify based on subfamilies [family]
   -h|--help            : this message
-
-OUTPUTS
 \n";
 
 ## input
 my (
-  $RM_infile,
-  $fasta_infile,
   $mapping_infile,
-  $glob,
+  $gzip,
   $no_condense,
   $help, $debug
 );
@@ -39,46 +35,61 @@ my (
 my $outprefix = "repeats_tab";
 
 GetOptions (
-  'i|infile=s' => \$RM_infile,
-  'f|fasta:s' => \$fasta_infile,
-  'm|mapping:s' => \$mapping_infile,
-  'g|glob' => \$glob,
-  'h|help' => \$help,
-  'debug' => \$debug
+  'm|mapping=s' => \$mapping_infile,
+  'z|gz'        => \$gzip,
+  'h|help'      => \$help,
+  'debug'       => \$debug
 );
 ## help and usage
 die $usage if $help;
+die $usage unless ( $mapping_infile );
+
 print STDERR "[####] TE-EVOLUTION parse_RM_out.pl\n";
 print STDERR "[####] " . `date`;
-
-## custom order to print results
-my @sort_order = qw (
- Unknown Other
- DNA DNA/Academ DNA/CMC DNA/Crypton DNA/Dada DNA/Ginger DNA/IS3EU DNA/IS5 DNA/Kolobok DNA/MULE DNA/Maverick DNA/Merlin DNA/P DNA/PIF DNA/PiggyBac DNA/Sola DNA/TcMar DNA/Zator DNA/hAT
- RC RC/Helitron
- LTR LTR/Copia LTR/DIRS LTR/Gypsy LTR/Juno LTR/Mag LTR/Ngaro LTR/Pao LTR/TelKA
- LINE/Penelope
- LINE LINE/CR1 LINE/CRE LINE/Dong LINE/I LINE/L1 LINE/L2 LINE/Proto2 LINE/R1 LINE/R2 LINE/RTE LINE/Rex LINE/Soliton
- SINE SINE/5S SINE/I SINE/R2 SINE/tRNA
- tRNA rRNA Satellite Simple_repeat Low_complexity ARTEFACT RNA Retroposon repeat
-);
-my $count = 0;
-my %position_of;
-$position_of{$_} = $count++ for @sort_order;
 
 ## open outfile
 open (my $OUTFILE, ">".$outprefix.".txt") or die $!;
 
-###############
-## single files
-###############
-if ($RM_infile) {
+## gunzip the files
+if ( $gzip ) {
+  my @gzipped_files = glob "*.out.gz";
+  print STDERR "[INFO] Gunzip on ".scalar(@gzipped_files)." files\n";
+  if (system ("parallel --help &>/dev/null") != 0){
+    foreach my $file (@gzipped_files) {
+      `gunzip $file`;
+    }
+  } else {
+    `ls *.out.gz | parallel gunzip {}`;
+  }
+}
 
+## stuff
+my %files_hash;
+my %repeats_hash;
+my %genome_lengths_hash;
+
+my @files = glob "*.out";
+print STDERR "[INFO] Number of RM files: ".@files."\n";
+
+## open mapping file to get genome sizes
+open (my $MAP, $mapping_infile) or die $!;
+while (<$MAP>) {
+  chomp;
+  my @F = split (m/\s+/, $_);
+  ## filename (*.out) in col1; genome span in col2
+  $genome_lengths_hash{$F[0]} = $F[1];
+}
+close $MAP;
+
+foreach my $file (@files) {
+  print STDERR "[INFO] Working on file: $file\n";
   ## STUFF
   my %print_hash;
+  (my $fasta_infile = $file) =~ s/\.out//;
   my $genome_length;
 
-  if ($fasta_infile) {
+  ## open fasta only if mapping file not provided
+  unless ( $mapping_infile ) {
     open (my $FASTA, $fasta_infile) or die $!;
     while (<$FASTA>) {
       if ($_ =~ m/^\>/) {
@@ -89,10 +100,14 @@ if ($RM_infile) {
       }
     }
     close $FASTA;
-    print STDERR "[####] Genome length: $genome_length bp\n";
-  }
 
-  open (my $IN, $RM_infile) or die $!;
+    print STDERR "[INFO] Genome length: $genome_length bp\n";
+    $genome_lengths_hash{$file} = $genome_length;
+} else {
+  print STDERR "[INFO] Genome length: $genome_lengths_hash{$file} bp\n";
+}
+
+  open (my $IN, $file) or die $!;
   while (<$IN>) {
     chomp;
     my $line = trim ($_); ## remove leading/trailing whitespace
@@ -106,106 +121,42 @@ if ($RM_infile) {
         $rep =~ s/\?//g;
       }
       $print_hash{$rep} += ( $F[6] - $F[5] + 1 );
+      $repeats_hash{$rep}++;
     }
   }
   close $IN;
+  $files_hash{$file} = \%print_hash;
+}
 
-  foreach (sort { "\L$a" cmp "\L$b" || $print_hash{$b} <=> $print_hash{$a} } keys %print_hash) {
-    print $OUTFILE join ("\t", $_, $print_hash{$_});
-    if ($fasta_infile) {
-      print $OUTFILE "\t" . (($print_hash{$_}/$genome_length)*100) . "\n";
+print $OUTFILE join ("\t", "repeat", nsort keys %files_hash) . "\n";
+foreach my $repeat (nsort keys %repeats_hash) {
+  print $OUTFILE "$repeat\t";
+  foreach my $file (nsort keys %files_hash) {
+    my %print_hash = %{ $files_hash{$file} };
+    if ($print_hash{$repeat} ) {
+      print $OUTFILE (($print_hash{$repeat}/$genome_lengths_hash{$file})*100) . "\t";
     } else {
-      print $OUTFILE "\n";
+      print $OUTFILE "0\t";
     }
   }
-
-######################
-## glob multiple files
-######################
-} elsif ($glob) {
-
-  ## STUFF
-  my %files_hash;
-  my %repeats_hash;
-  my %genome_lengths_hash;
-
-  my @files = glob "*.out";
-  print STDERR "[####] Number of RM files: ".@files."\n";
-
-  if ( $mapping_infile ) {
-    open (my $MAP, $mapping_infile) or die $!;
-    while (<$MAP>) {
-      chomp;
-      my @F = split (m/\s+/, $_);
-      ## filename (*.out) in col1; genome span in col2
-      $genome_lengths_hash{$F[0]} = $F[1];
-    }
-    close $MAP;
-  }
-
-  foreach my $file (@files) {
-    print STDERR "[####] Working on file: $file\n";
-    ## STUFF
-    my %print_hash;
-    (my $fasta_infile = $file) =~ s/\.out//;
-    my $genome_length;
-
-    ## open fasta only if mapping file not provided
-    unless ( $mapping_infile ) {
-      open (my $FASTA, $fasta_infile) or die $!;
-      while (<$FASTA>) {
-        if ($_ =~ m/^\>/) {
-          next;
-        } else {
-          chomp;
-          $genome_length += length ($_);
-        }
-      }
-      close $FASTA;
-
-      print STDERR "[####] Genome length: $genome_length bp\n";
-      $genome_lengths_hash{$file} = $genome_length;
-  } else {
-    print STDERR "[####] Genome length: $genome_lengths_hash{$file} bp\n";
-  }
-
-    open (my $IN, $file) or die $!;
-    while (<$IN>) {
-      chomp;
-      my $line = trim ($_); ## remove leading/trailing whitespace
-      if ($line !~ m/^\d+/) {
-        next;
-      } else {
-        my @F = split (/\s+/, $line);
-        my $rep = $F[10];
-        unless ($no_condense) {
-          $rep =~ s/-.*//;
-          $rep =~ s/\?//g;
-        }
-        $print_hash{$rep} += ( $F[6] - $F[5] + 1 );
-        $repeats_hash{$rep}++;
-      }
-    }
-    close $IN;
-    $files_hash{$file} = \%print_hash;
-  }
-
-  print $OUTFILE join ("\t", "repeat", nsort keys %files_hash) . "\n";
-  foreach my $repeat (nsort keys %repeats_hash) {
-    print $OUTFILE "$repeat\t";
-    foreach my $file (nsort keys %files_hash) {
-      my %print_hash = %{ $files_hash{$file} };
-      if ($print_hash{$repeat} ) {
-        print $OUTFILE (($print_hash{$repeat}/$genome_lengths_hash{$file})*100) . "\t";
-      } else {
-        print $OUTFILE "0\t";
-      }
-    }
-    print $OUTFILE "\n";
-  }
+  print $OUTFILE "\n";
 }
 
 close $OUTFILE;
+
+## gunzip the files
+if ( $gzip ) {
+  print STDERR "[INFO] Gzip on ".scalar(@files)." files\n";
+  if (system ("parallel --help &>/dev/null") != 0){
+    foreach my $file (@files) {
+      `gzip $file`;
+    }
+  } else {
+    `ls *.out | parallel gzip {}`;
+  }
+}
+
+print STDERR "[INFO] Finished! " . `date`;
 
 ############ SUBS
 
@@ -214,12 +165,5 @@ sub trim {
   $s =~ s/^\s+|\s+$//g;
   return $s
 }
-
-sub by_order {
-#  my @a = split m/\//, $a;
-#  my @b = split m/\//, $b;
-  $position_of{$a} <=> $position_of{$b};
-}
-
 
 __END__
