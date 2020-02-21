2b76454070d45ead624488884e086b8266b4178d
Reuben Nowell <reubennowell@gmail.com>
2020-02-21 16:14:30 +0100

    add blast_tags2

--- blast_tags2.pl
+++ blast_tags2.pl
@@ -63,12 +63,12 @@ GetOptions (
 ## help and usage
 die $usage if $help;
 die $usage unless ( $IN_path && $db_string );
+$IN_path =~ s/\/$//;
 
 print STDERR "[####] TE-EVOLUTION blast_tags.pl\n";
 print STDERR "[####] " . `date`;
 ##
 my ( %ltr_hash, %results );
-my ( $ambiguous ) = ( 0 );
 
 ## check system for required programs
 check_progs();
@@ -93,49 +93,6 @@ print STDERR "[INFO] Check/make blastdb's for: @databases_makedb\n";
 @databases_makedb = @{ check_blastdbs(\@databases_makedb) }; ## check which database files need makeblastdb run on them
 make_blastdbs( \@databases_makedb ); ## and then do it
 
-# ## parse SAM input file
-# ## to generate te-tags with metadata
-# open (my $SAM, "samtools view $IN_file |") or die $!;
-# while (my $line = <$SAM>) {
-#   chomp $line;
-#   my @F = split ("\t", $line);
-#   ## parse reads and build hash
-#   if ( $F[5] =~ m/^(\d+)S/ ) {
-#     ## read overhangs left-side of LTR element
-#     if ( $1 >= $overhang_threshold ) {
-#       ## read has te-tag >= $overhang_threshold
-#       $F[0] =~ s/\s.+// if ($F[0] =~ m/\s+/); ## remove trailing text with whitespaces, common in fastq headers
-#       push @{ $ltr_hash{$F[2]}{left_names} }, $F[0];
-#       push @{ $ltr_hash{$F[2]}{left_cigars} }, $F[5];
-#       push @{ $ltr_hash{$F[2]}{left_seqs} }, $F[9];
-#
-#     }
-#   } elsif ( $F[5] =~ m/(\d+)S$/ ) {
-#     ## read overhangs right-side of LTR element
-#     if ( $1 >= $overhang_threshold ) {
-#       ## read has te-tag >= $overhang_threshold
-#       $F[0] =~ s/\s.+// if ($F[0] =~ m/\s+/);
-#       push @{ $ltr_hash{$F[2]}{right_names} }, $F[0];
-#       push @{ $ltr_hash{$F[2]}{right_cigars} }, $F[5];
-#       push @{ $ltr_hash{$F[2]}{right_seqs} }, $F[9];
-#
-#     }
-#
-#   } else {
-#     print STDERR "[INFO] Skipping read $F[0] due to ambiguous cigar $F[5]\n" if ( $verbose );
-#     $ambiguous++;
-#   }
-# }
-# close $SAM;
-#
-# print Dumper \%ltr_hash if ( $debug );
-#
-# ## make dir structure for sequences
-# my $base_dir = $OUT_prefix . "_sequences";
-# unlink $base_dir if (-d $base_dir); ## delete it if exists, it's a brutal world
-# system mkdir => '-p' => "$base_dir/lefties";
-# system mkdir => '-p' => "$base_dir/righties";
-
 ## open BLAST results file
 my $blast_file = $OUT_prefix . "_blast.txt";
 open (my $BLAST, ">$blast_file") or die $!;
@@ -146,7 +103,7 @@ my @fasta_files = glob ("$IN_path/*fasta $IN_path/*fnaa $IN_path/*fa");
 print STDERR "[INFO] There are ".scalar(@fasta_files)." files in '$IN_path'\n";
 
 ## iterate across files and dbs; do BLAST
-foreach my $fasta_file (@fasta_files) {
+foreach my $fasta_file (nsort @fasta_files) {
 
   ## load $ltr_hash
   (my $repeat_id = $fasta_file) =~ s/\.[^.]+$//;
@@ -202,121 +159,6 @@ foreach my $fasta_file (@fasta_files) {
   }
 }
 close $BLAST;
-
-print Dumper(\%ltr_hash);
-# ## print lefties and righties to file for BLASTing; do BLAST
-# foreach my $query (nsort keys %ltr_hash) {
-#
-#   ## ~~~~~~~~~~~~~~~
-#   ## process lefties
-#   ## ~~~~~~~~~~~~~~~
-#   if ( ($ltr_hash{$query}{left_names}) && ($ltr_hash{$query}{left_seqs}) ) {
-#     my @names_arr = @{$ltr_hash{$query}{left_names}};
-#     my @seqs_arr = @{$ltr_hash{$query}{left_seqs}};
-#     my @cigars_arr = @{$ltr_hash{$query}{left_cigars}};
-#     my @overhangs_arr = map { m/^(\d+)S/; $1 } @cigars_arr;
-#     my %cigars; @cigars{@names_arr} = @cigars_arr; ##key= seqid; val= cigar
-#     my %boundaries; @boundaries{@names_arr} = @overhangs_arr; ##key= seqid; val=overhang value from cigar
-#     print Dumper \%boundaries if ( $debug );
-#
-#     ## write to file
-#     my $query_file = "$base_dir/lefties/$query.fa";
-#     open (my $F, ">$query_file") or die $!;
-#     for my $i ( 0 .. $#names_arr ) {
-#       print $F ">$names_arr[$i]\n$seqs_arr[$i]\n";
-#     }
-#     close $F;
-#
-#     ## BLAST vs each database in turn
-#     foreach my $database (@databases_blastdb) {
-#       print STDERR "[INFO] BLASTing query '$query' LEFT TE-tags versus database '$database'...\n";
-#
-#       ## open blast filehandle
-#       open (my $IN, "blastn -task megablast -num_threads $threads -evalue $evalue -query $query_file -db $database -outfmt '6 std qcovhsp' |") or die $!;
-#
-#       ## iterate thru blast results
-#       while (my $line = <$IN>) {
-#         chomp $line;
-#         my ($qacc, $sacc, $pident, $length, $mismatch, $gapopen, $qstart, $qend, $sstart, $send, $evalue, $bitscore, $qcovhsp) = split( m/\s+/, $line );
-#
-#         my $score;
-#         if ( $qcovhsp == 100 ) { ## successful alignment across entire tag
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 1;
-#           ## annotate BLAST result
-#           print $BLAST join ("\t", $line,$query,"L",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"full","PASS",$score) . "\n";
-#
-#         } elsif ( ($qstart < ($boundaries{$qacc}-$match_threshold)) && ($qend > ($boundaries{$qacc}+$match_threshold)) ) { ## successful match spanning +/- $match_threshold over TE/genome boundary
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 0.8;
-#           ## annotate BLAST result
-#           print $BLAST join ("\t", $line,$query,"L",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"partial","PASS",$score) . "\n";
-#
-#         } elsif ( ($qstart <= $boundaries{$qacc}) && ($qend => $boundaries{$qacc}) ) { ## successful match spanning TE/genome boundary by any margin
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 0.2;
-#           $score = 0 if ( $collapse_marginal_scores ); ## collapse marginal calls to score = 0
-#           ## annotate BLAST result
-#           print $BLAST join ("\t", $line,$query,"L",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"partial","MARGINAL",$score) . "\n";
-#
-#         } else { ## match that does not span TE/genome boundary by any overlap
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 0;
-#           $score = 0 if ( $collapse_marginal_scores ); ## collapse marginal calls to score = 0
-#           ## annotate BLAST result
-#           print $BLAST join ("\t", $line,$query,"L",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"partial","FAIL",$score) . "\n";
-#         }
-#       }
-#       close $IN;
-#     }
-#   }
-#
-#   ## ~~~~~~~~~~~~~~~~
-#   ## process righties
-#   ## ~~~~~~~~~~~~~~~~
-#   if ( ($ltr_hash{$query}{right_names}) && ($ltr_hash{$query}{right_seqs}) ) {
-#     my @names_arr = @{$ltr_hash{$query}{right_names}};
-#     my @seqs_arr = @{$ltr_hash{$query}{right_seqs}};
-#     my @cigars_arr = @{$ltr_hash{$query}{right_cigars}};
-#     my @overhangs_arr = map { m/(\d+)S$/; $1 } @cigars_arr;
-#     my %cigars; @cigars{@names_arr} = @cigars_arr; ##key= seqid; val= cigar
-#     my %boundaries; @boundaries{@names_arr} = @overhangs_arr; ##key= seqid; val=overhang value from cigar
-#     print Dumper \%boundaries if ( $debug );
-#
-#     ## write to file
-#     my $query_file = "$base_dir/righties/$query.fa";
-#     open (my $F, ">$query_file") or die $!;
-#     for my $i ( 0 .. $#names_arr ) {
-#       print $F ">$names_arr[$i]\n$seqs_arr[$i]\n";
-#     }
-#     close $F;
-#
-#     ## BLAST vs each database in turn
-#     foreach my $database (@databases_blastdb) {
-#       print STDERR "[INFO] BLASTing query '$query' RIGHT TE-tags versus database '$database'...\n";
-#       open (my $IN, "blastn -task megablast -num_threads $threads -evalue $evalue -query $query_file -db $database -outfmt '6 std qcovhsp' |") or die $!;
-#       while (my $line = <$IN>) {
-#         chomp $line;
-#         my ($qacc, $sacc, $pident, $length, $mismatch, $gapopen, $qstart, $qend, $sstart, $send, $evalue, $bitscore, $qcovhsp) = split( m/\s+/, $line );
-#         my $score;
-#         if ( $qcovhsp == 100 ) { ## successful alignment across entire tag
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 1;
-#           print $BLAST join ("\t", $line,$query,"R",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"full","PASS",$score) . "\n";
-#         } elsif ( ($qstart < ($boundaries{$qacc}-$match_threshold)) && ($qend > ($boundaries{$qacc}+$match_threshold)) ) { ## successful match spanning +/- $match_threshold over TE/genome boundary
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 0.8;
-#           print $BLAST join ("\t", $line,$query,"R",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"partial","PASS",$score) . "\n";
-#         } elsif ( ($qstart <= $boundaries{$qacc}) && ($qend => $boundaries{$qacc}) ) { ## successful match spanning TE/genome boundary by any margin
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 0.2;
-#           $score = 0 if ( $collapse_marginal_scores ); ## collapse marginal calls to score = 0
-#           print $BLAST join ("\t", $line,$query,"R",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"partial","MARGINAL",$score) . "\n";
-#         } else { ## match that does not span TE/genome boundary by any overlap
-#           $score = ( $use_qcovhsp_as_score ) ? $qcovhsp : 0;
-#           $score = 0 if ( $collapse_marginal_scores ); ## collapse marginal calls to score = 0
-#           print $BLAST join ("\t", $line,$query,"R",$databases_names{$database},$cigars{$qacc},$boundaries{$qacc},"partial","FAIL",$score) . "\n";
-#         }
-#       }
-#       close $IN;
-#     }
-#   }
-# }
-# close $BLAST;
-# print Dumper \%results;
 
 ## process annotated blast results
 ## want to save the 'best' score per query-subject pair only
