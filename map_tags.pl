1291f5d9a284b9c435bb719f97b47ca2b032c2c7
Reuben Nowell <reubennowell@gmail.com>
2020-06-15 14:08:21 +0200

    bug fix

--- map_tags.pl
+++ map_tags.pl
@@ -181,11 +181,11 @@ foreach my $repeat_id ( nsort keys %repeat_hash ) {
   foreach my $database ( nsort keys %sam_hash ) {
     my @scores = qw/0/;
     my @counts = qw/0/;
-    foreach my $ltr_id ( nsort keys %{$sam_hash{$databases_names{$database}}{$repeat_id}} ) {
-      foreach my $score ( (sort {$b<=>$a} keys %{$sam_hash{$databases_names{$database}}{$repeat_id}{$ltr_id}})[0] ) { ## top hit!
-        print STDOUT join ("\t", "TOPHIT:",$databases_names{$database},$repeat_id,$ltr_id,$score,$sam_hash{$databases_names{$database}}{$repeat_id}{$ltr_id}{$score}) . "\n" if ($debug);
+    foreach my $ltr_id ( nsort keys %{$sam_hash{$database}{$repeat_id}} ) {
+      foreach my $score ( (sort {$b<=>$a} keys %{$sam_hash{$database}{$repeat_id}{$ltr_id}})[0] ) { ## top hit!
+        print STDOUT join ("\t", "TOPHIT:",$database,$repeat_id,$ltr_id,$score,$sam_hash{$database}{$repeat_id}{$ltr_id}{$score}) . "\n" if ($debug);
         push (@scores, $score);
-        push (@counts, $sam_hash{$databases_names{$database}}{$repeat_id}{$ltr_id}{$score});
+        push (@counts, $sam_hash{$database}{$repeat_id}{$ltr_id}{$score});
       }
     }
     print $SCORES "\t" . sum(@scores)/200;
